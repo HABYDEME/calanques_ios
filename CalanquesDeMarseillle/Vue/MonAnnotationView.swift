6ef9380246b4c07077bcf41afe3d075efa60f4a6
HABY DEME <HABYTIMAH@OUTLOOK.FR>
2020-05-09 12:06:06 +0200

    CreateAnnotationsView

---
+++ CalanquesDeMarseillle/Vue/MonAnnotationView.swift
@@ -0,0 +1,47 @@
+//
+//  MonAnnotationView.swift
+//  CalanquesDeMarseillle
+//
+//  Created by DEMEHABY on 09/05/2020.
+//  Copyright © 2020 Deme Haby. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MonAnnotationView: MKAnnotationView {
+
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        setupAnnotation()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupAnnotation()
+    }
+    func setupAnnotation() {
+        image = UIImage(named: "placeholder")
+        canShowCallout = true
+        
+        leftCalloutAccessoryView = setupLeft()
+        rightCalloutAccessoryView = setupRight()
+        detailCalloutAccessoryView = setupCenter()
+     
+    }
+    func setupLeft() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(UIImage(named: "distance"), for: .normal)
+        return button
+    }
+    func setupRight() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(UIImage(named: "detail"), for: .normal)
+        return button
+    }
+    func setupCenter() -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 125, height: 125))
+        view.backgroundColor = .red
+        return view
+    }
+    
+}
